ae5296429b91d4160225c03ba9e1a47c243e538f
Abhishek <abhishekmore021@gmail.com>
2021-03-18 12:06:42 +0530

    added MONGO DB

--- server.js
+++ server.js
@@ -4,20 +4,30 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config({ path: 'config.env' });
 const port = process.env.PORT || 8080;
 
+// log request
 app.use(morgan('tiny'));
 
+// mongoDB connection
+connectDB();
+
+// parse request to body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// set view engine
 app.set("view engine", "ejs");
 
+// load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
 
+// load routers
 app.use('/', require('./server/routes/router'));
 
 app.listen(port, () => console.log(`server running on localhost:${port}`));
