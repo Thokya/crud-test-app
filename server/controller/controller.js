06b5ce0f376efd77e86ff801b3fd91b1333967e3
Abhishek <abhishekmore021@gmail.com>
2021-03-18 12:31:10 +0530

    API now able to create and save new category entry

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,8 +1,31 @@
+const CatDB = require('../model/model');
 let Catdb = require('../model/model');
 
 // create and save new category
 exports.create = (req, res) => {
+    // validate request
+    if (!req.body) {
+        res.status(400).send({ message: "Cannot be empty!" })
+        return;
+    }
 
+    // create new category
+    const category = new CatDB({
+        id: req.body.id,
+        name: req.body.name
+    });
+
+    // saving category in DB
+    category
+        .save(category)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(e => {
+            res.status(500).send({
+                message: e.message || "Some error has occured while create operation"
+            });
+        });
 }
 
 // show all category/single category
